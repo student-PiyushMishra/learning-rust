8b748ce007a33c22e0d84fb25f3c758d95cd5e4f
Piyush Mishra <student.piyushmishra@gmail.com>
2025-07-18 06:20:20 +0530

    refactored the code

--- fibonacci_series/src/main.rs
+++ fibonacci_series/src/main.rs
@@ -1,19 +1,15 @@
-fn main() {
-    println!("{}", fib(3));
+fn main () {
+    println!("{}",fib(6));
 }
 
 fn fib(num: i32) -> i32 {
     let mut first = 0;
     let mut second = 1;
-    if num == 0 {
-        return first;
-    }
-    if num == 1 {
-        return second;
-    }
-    for _i in 1..num - 2 {
+    if num == 0 {return 0}
+    if num == 1 {return 1}
+    for _i in 2..=num {
         let temp = second;
-        second = second + first;
+        second = first + second;
         first = temp;
     }
     return second;
