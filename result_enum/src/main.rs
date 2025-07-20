349475d6b8790e5a9294e6d946eb3c98fecf7c0a
Piyush Mishra <student.piyushmishra@gmail.com>
2025-07-20 19:01:43 +0530

    check

--- result_enum/src/main.rs
+++ result_enum/src/main.rs
@@ -1,9 +1,9 @@
-use std::fs::read_to_string;
+use std::{fs::read_to_string};
 
 fn main() {
-    let result = read_to_string("./a");
+    let result = read_to_string("./a.txt");
     match result {
         Ok(output) => println!("{}",output),
         Err(error) => println!("Error while reading the file! \n {}",error)
-    }
+    }    
 }
