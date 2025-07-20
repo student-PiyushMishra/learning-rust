2d28575622da86cd72b68da6e6e469911a0ed067
Piyush Mishra <student.piyushmishra@gmail.com>
2025-07-20 08:04:54 +0530

    result enum

--- result_enum/src/main.rs
+++ result_enum/src/main.rs
@@ -1,9 +1,9 @@
 use std::fs::read_to_string;
 
 fn main() {
-    let output_of_file = read_to_string("./a"); // if file "a" is present, it logs its content else logs error
-    match output_of_file {
-        Ok(text) => println!("{}", text),
-        Err(e) => println!("Error while reading the file... \n {}", e),
-    };
+    let result = read_to_string("./a");
+    match result {
+        Ok(output) => println!("{}",output),
+        Err(error) => println!("Error while reading the file! \n {}",error)
+    }
 }
